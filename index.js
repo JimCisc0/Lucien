d571dbf433f57b254c2b42e1ba4a5f50606207cc
JimCisc0 <killiankaeses@gmail.com>
2018-05-25 14:46:31 +0200

    -Ajout commande Kick -Ajout commande Ban

--- index.js
+++ index.js
@@ -1,33 +1,87 @@
 const botconfig = require("./botconfig.json");
 const tokenfile = require("./token.json");
 const Discord = require("discord.js");
-
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on('ready', async () => {
   console.log(`${bot.user.username} is online`);
-  bot.user.setActivity("surveiller Lucie");
+  bot.user.setActivity("surveiller Lucie", "WATCHING");
 });
 
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  // !hello
 
-  // if(cmd === `${prefix}hello`){
-  //   return message.channel.send("Hello!")
-  // }
+//            KICK
+
+
+  if(cmd === `${prefix}kick`){
+
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Tu n'as pas les droits boloss");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Cette personne ne peut être kick");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser}`)
+    .addField("Kicked By", `<@${message.author.id}>`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "log");
+    if(!kickChannel) return message.channel.send("Je ne peux pas trouver le chan log");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+    return;
+  }
+
+  //                            BAN
+
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Tu n'as pas les droits boloss");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Cette personne ne peut être kick");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser} `)
+    .addField("Banned By", `<@${message.author.id}> `)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "log");
+    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
+
+
+    return;
+  }
+
+// SERVER INFO
 
   if(cmd === `${prefix}serverinfo`){
 
-    let sicon = message.guild.iconURL; 
+    let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
     .setDescription("Server Information")
     .setColor("#15f153")
@@ -39,6 +93,10 @@ bot.on("message", async message => {
 
     return message.channel.send(serverembed);
   }
+
+
+// BOT INFO
+
 
   if(cmd === `${prefix}botinfo`){
 
