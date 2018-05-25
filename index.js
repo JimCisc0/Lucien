fe18f32872d4daeb3b65db6e877b2887decb95a0
JimCisc0 <killiankaeses@gmail.com>
2018-05-25 10:53:58 +0200

    -Création du fichier token séparément de la config du bot -Création cmd botinfo -Création cmd serverinfo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const botconfig = require ("./botconfig.json");
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
@@ -20,11 +21,43 @@ bot.on("message", async message => {
 
   // !hello
 
-  if(cmd === `${prefix}hello`){
-    return message.channel.send("Hello!")
+  // if(cmd === `${prefix}hello`){
+  //   return message.channel.send("Hello!")
+  // }
+
+  if(cmd === `${prefix}serverinfo`){
+
+    let sicon = message.guild.iconURL; 
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Nom du serveur", message.guild.name)
+    .addField("Créé le", message.guild.createdAt)
+    .addField("Tu as rejoins le", message.member.joinedAt)
+    .addField("Nombre total de membres:", message.guild.memberCount);
+
+    return message.channel.send(serverembed);
+  }
+
+  if(cmd === `${prefix}botinfo`){
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#1b3dc4")
+    .setThumbnail(bicon)
+    .setAuthor("Lucien, le diable qui vous observe !")
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+
+
+    return message.channel.send(botembed);
+
   }
 
 
-})
+});
 
-bot.login(botconfig.token);
+bot.login(tokenfile.token);
